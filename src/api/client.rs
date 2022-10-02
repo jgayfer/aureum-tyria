c15dde023a1494e1bef91ccaf056a4dde1be92b0
James Gayfer <gayfer.james@gmail.com>
2022-10-02 13:10:53 -0700

    Document API client
    Rather than requiring the user to go read the API docs, let's do our
    best to provide some indicate as to what these methods do.

--- src/api/client.rs
+++ src/api/client.rs
@@ -4,11 +4,13 @@ use super::model::{ItemListings, ItemPrice};
 
 const BASE_URL: &str = "https://api.guildwars2.com/v2";
 
+/// An API client for interacting with the Guild Wars 2 API.
 pub struct Client {
     base_url: String,
 }
 
 impl Default for Client {
+    /// Build a ready to use [Client].
     fn default() -> Self {
         Self {
             base_url: BASE_URL.to_string(),
@@ -17,15 +19,21 @@ impl Default for Client {
 }
 
 impl Client {
+    /// Build a new [Client] with a different base URL.
     fn new(base_url: String) -> Self {
         Self { base_url }
     }
 
+    /// Get current buy and sell listings for an item from the trading post.
     async fn item_listings(self, item_id: u32) -> Result<ItemListings> {
         let url = format!("{}/commerce/listings/{}", self.base_url, item_id);
         Ok(reqwest::get(url).await?.json::<ItemListings>().await?)
     }
 
+    /// Get current prices for an item from the trading post.
+    ///
+    /// For more detailed information about the distribution of list prices, use
+    /// [Client::item_listings].
     async fn item_prices(self, item_id: u32) -> Result<ItemPrice> {
         let url = format!("{}/commerce/prices/{}", self.base_url, item_id);
         Ok(reqwest::get(url).await?.json::<ItemPrice>().await?)
