f4d59ec50412e72daa864be986b9e1ea31168987
James Gayfer <gayfer.james@gmail.com>
2022-10-12 22:53:19 -0700

    Make item price data cloneable
    This is probably because I'm bad at ownership.

--- src/item.rs
+++ src/item.rs
@@ -1,11 +1,11 @@
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct ItemPrice {
     pub item_id: u32,
     pub buy_listings: Vec<ListEntry>,
     pub sell_listings: Vec<ListEntry>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct ListEntry {
     pub listing_count: u32,
     pub unit_price: u32,
